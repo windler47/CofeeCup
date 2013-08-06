48c9a01b6c53c9df597b6afc98a588eb73598bac
Maximov A Alexandr <scoundrelalva@gmail.com>
2013-08-07 00:43:08 +0400

    New XML Parser

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -4,42 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace testApp
 {
-    class Customer
-    {
-        public string Name;
-        public string Address;
-        public string RGBCustomerID;
-        public string RicoCustomerID;
-    }
-    class Product
-    {
-
-    }
-    class Invoice
-    {
-
-    }
+    //class Customer
+    //{
+    //    public string Name;
+    //    public string Address;
+    //    public string RGBCustomerID;
+    //    public string RicoCustomerID;
+    //}
+    //class Product
+    //{
+    //    public string Name;
+    //    public string RGBProductID;
+    //    public string RicoProductID;
+    //}
+    //class Invoice
+    //{
+    //    public Customer  a;
+    //}
     class Program
     {
         static void Main(string[] args)
         {
-            XmlDocument xmlrDoc = new XmlDocument();
-            XmlDocument xmlwDoc = new XmlDocument();
-            xmlrDoc.Load(args[0]);
-            foreach (XmlNode table in xmlrDoc.DocumentElement.ChildNodes)
-            {
-                Console.Write(table.Name);
-                foreach (XmlAttribute attr in table.Attributes)
-                {
-                    Console.Write(attr.Name); Console.Write(" ");
-                }
-                Console.WriteLine();
-            }
-            xmlwDoc.CreateXmlDeclaration("1.0", null, null);
+            string testxml = "020813РБУ.xml";
+            XElement xmlDoc = XElement.Load(testxml);
+            Dictionary<int, string> goods = GetGoods(xmlDoc);        
+        }
+        public static Dictionary<int, string> GetGoods(XElement xmlDoc)
+        {
+            Dictionary<int,string> result = new Dictionary<int,string>();
+            IEnumerable<XElement> goods =
+                from fobject in xmlDoc.Elements("Объект")
+                where (string)fobject.Attribute("ИмяПравила") == "Номенклатура"
+                select fobject;
 
+            foreach (XElement el in goods)
+            {
+                #region Qery data from Xml
+                bool isGroup = (from el1 in el.Element("Ссылка").Elements("Свойство")
+                                where (string)el1.Attribute("Имя") == "ЭтоГруппа" && (string)el1.Element("Значение") == "true"
+                                select el1).Any();
+                if (isGroup) continue;
+                string productName = (string)(from el1 in el.Elements("Свойство")
+                                              where (string)el1.Attribute("Имя") == "Наименование"
+                                              select el1).ElementAt(0);
+                #endregion
+                //Console.WriteLine("{0} Товар: {1}", el.Element("Ссылка").Attribute("Нпп"), productName);
+                result.Add((int)el.Element("Ссылка").Attribute("Нпп"),productName);
+            }         
+            return result;
         }
     }
 }
