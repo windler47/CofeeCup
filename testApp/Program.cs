289a9e1efe48a1c3507f06b1e1db899e158937a5
Maximov A Alexandr <scoundrelalva@gmail.com>
2013-08-07 03:50:11 +0400

    GetCustomers function added

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -8,30 +8,28 @@ using System.Xml.Linq;
 
 namespace testApp
 {
-    //class Customer
-    //{
-    //    public string Name;
-    //    public string Address;
-    //    public string RGBCustomerID;
-    //    public string RicoCustomerID;
-    //}
-    //class Product
-    //{
-    //    public string Name;
-    //    public string RGBProductID;
-    //    public string RicoProductID;
-    //}
-    //class Invoice
-    //{
-    //    public Customer  a;
-    //}
+    class Customer
+    {
+        public string Name;
+        public string City;
+        public Customer(string name)
+        {
+            Name = name;
+        }
+        public Customer(string name, string city)
+        {
+            Name = name;
+            City = city;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             string testxml = "020813РБУ.xml";
             XElement xmlDoc = XElement.Load(testxml);
-            Dictionary<int, string> goods = GetGoods(xmlDoc);        
+            Dictionary<int, string> goods = GetGoods(xmlDoc);
+            Dictionary<int, Customer> customers = GetCustomers(xmlDoc);
         }
         public static Dictionary<int, string> GetGoods(XElement xmlDoc)
         {
@@ -55,6 +53,26 @@ namespace testApp
                 //Console.WriteLine("{0} Товар: {1}", el.Element("Ссылка").Attribute("Нпп"), productName);
                 result.Add((int)el.Element("Ссылка").Attribute("Нпп"),productName);
             }         
+            return result;
+        }
+        public static Dictionary<int, Customer> GetCustomers(XElement xmlDoc)
+        {
+            Dictionary<int, Customer> result = new Dictionary<int, Customer>();
+            IEnumerable<XElement> obj =
+                from fobject in xmlDoc.Elements("Объект")
+                where (string)fobject.Attribute("ИмяПравила") == "Контрагенты"
+                select fobject;
+            foreach (XElement el in obj)
+            {
+                bool isGroup = (from el1 in el.Element("Ссылка").Elements("Свойство")
+                                where (string)el1.Attribute("Имя") == "ЭтоГруппа" && (string)el1.Element("Значение") == "true"
+                                select el1).Any();
+                if (isGroup) continue;
+                Customer customer = new Customer((string)(from el1 in el.Elements("Свойство")
+                                              where (string)el1.Attribute("Имя") == "Наименование"
+                                              select el1).ElementAt(0));
+                result.Add((int)el.Element("Ссылка").Attribute("Нпп"),customer);
+            }
             return result;
         }
     }
